d55df719600c98245e81a2f0eff80adcaa4fe811
Matias Leonardo Baez <elmattprofe@gmail.com>
2024-05-29 21:00:24 -0300

    ticket #00003 REFACTOR
    + loginController.php se aplica la tecnica de plantilla con carga por fopen
    + landingView.html se agrego el link para acceder a la seccion de login

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,5 +1,18 @@
 <?php 
 
-	include_once 'views/loginView.html';
+	// carga de plantilla
+	$f_view = fopen('views/loginView.html', "r");
+
+	$buffer = "";
+
+	while (!feof($f_view)) {
+	 	$buffer .= fgets($f_view);
+	} 
+		
+	// modificacion de la plantilla
+
+
+	// imprimir en pantalla la plantilla
+	echo $buffer;
 
  ?>
